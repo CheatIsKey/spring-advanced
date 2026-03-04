adcf104956659b85b04e9568f8233a0ce40cd8a2
재민 <silvull2t@naver.com>
2026-03-04 11:17:13 +0900

    fix: ArgumentResolver를 Bean으로 등록하고, Configuration을 통해 스프링에게 조립하도록 알린다.

--- src/main/java/org/example/expert/config/PersistenceConfig.java
+++ src/main/java/org/example/expert/config/PersistenceConfig.java
@@ -2,8 +2,23 @@ package org.example.expert.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableJpaAuditing
-public class PersistenceConfig {
+public class PersistenceConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    public PersistenceConfig(AuthUserArgumentResolver authUserArgumentResolver) {
+        this.authUserArgumentResolver = authUserArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
+    }
 }
